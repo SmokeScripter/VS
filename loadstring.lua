30bff76c0fcb41eb919d9bb7c76069edd25937cb
SmokeScripter <111685137+SmokeScripter@users.noreply.github.com>
2022-08-22 01:01:41 +0100

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -5,6 +5,8 @@ function Vape.Module(Version, Is_Public)
 	if Is_Public == true then
 		return print("e")
 	elseif Is_Public == false then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Swoptt/Scripts/main/VPEASSETDOWNLOADER", true))()
+		wait(2)
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeScripter/RobloxScripts/main/Bedwars/Vape.lua", true))()
 	end
 	if Version == "V4" then
